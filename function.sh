bf905f463a067dc3be261e8ca12d2b8e72d76c71
Lady-N <ladynjola@gmail.com>
2024-07-25 09:48:20 -0400

    My first commit

---
+++ function.sh
@@ -0,0 +1,12 @@
+read -p "please enter a number: " num1
+read -p "please enter a second number: " num2
+
+if [ $num1 -eq $num2 ]; then
+    echo "numbers are equal"
+    exit 0
+else 
+    echo "numbers are not equal"
+    exit 1
+fi 
+}
+etech_check
